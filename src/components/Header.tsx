bfc572694e0f840bb087203634b458cadd9b54de
taroj1205 <taroj1205@gmail.com>
2023-09-28 09:17:48 +1300

    basic portal home page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,33 +1,87 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 
 const Header = () => {
+    const [informationOpen, setInformationOpen] = useState(false);
+    const [resultsOpen, setResultsOpen] = useState(false);
+    const dropdownRef = useRef<HTMLDivElement>(null);
+    const infoDropdownRef = useRef<HTMLDivElement>(null);
+    const resultsDropdownRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        const handleClickOutside = (event: MouseEvent) => {
+            if (infoDropdownRef.current && !infoDropdownRef.current.contains(event.target as Node)) {
+                setInformationOpen(false);
+            } if (resultsDropdownRef.current && !resultsDropdownRef.current.contains(event.target as Node)) {
+                setResultsOpen(false);
+            }
+        };
+
+        document.addEventListener('click', handleClickOutside);
+
+        return () => {
+            document.removeEventListener('click', handleClickOutside);
+        };
+    }, [dropdownRef]);
+
     return (
         <header className="bg-westlake relative">
-            <div className='max-w-6xl'>
-                <div className="flex flex-row justify-center space-x-12">
+            <div className='max-w-5xl mx-auto py-2'>
+                <div className="flex flex-row justify-between">
                     <div>
                         <Image className='w-80' src="/img/wbhs-logo.png" width={1600} height={491} alt="Westlake Logo" />
                     </div>
-                    <div className='flex flex-col'>
+                    <div className='flex flex-col max-w-[41.666%] w-full'>
                         <label className='text-white text-xs' htmlFor='username'>USERNAME: {' '} <span className='text-red-500'>*</span></label>
-                        <input className='w-full rounded-sm py-2' type='text' required name='username' placeholder='Username (Required)' />
+                        <input className='w-full outline-none rounded-md p-2 border focus:border-solid focus:border-blue-400' type='text' required name='username' placeholder='Username (Required)' />
                         <label className='text-white text-xs' htmlFor='password'>PASSWORD: {' '} <span className='text-red-500'>*</span></label>
-                        <div className='flex flex-row '>
-                            <input className='flex-shrink rounded-sm py-2' required type='password' name='password' placeholder='Password (Required)' />
-                            <button className='bg-login text-white py-2 px-4 rounded-r border-none' type='submit'>Sign on</button>
+                        <div className='flex flex-row w-full'>
+                            <input className='w-full outline-none rounded-l-md p-2 border focus:border-solid focus:border-blue-400' required type='password' name='password' placeholder='Password (Required)' />
+                            <button className='flex-shrink bg-login text-white py-2 px-4 rounded-r-md whitespace-nowrap' type='submit'>Sign on</button>
                         </div>
                     </div>
                 </div>
-                <nav className='flex flex-row justify-center items-center max-w-3xl text-white'>
-                    <div className='space-x-2'>
+                <nav className='flex flex-row justify-start py-4 items-center text-white'>
+                    <div className='flex space-x-4'>
                         <Link href='/'>Home</Link>
                         <Link href='/calendar'>Calendar</Link>
                         <Link href='/enrolment'>Enrolment</Link>
                         <Link href='/interviews'>Interviews</Link>
                         <Link href="/documents">Documents</Link>
                         <Link href="/help">Help</Link>
+                        <div className='flex flex-row space-x-2 dropdown' ref={dropdownRef}>
+                            <div className="relative" ref={infoDropdownRef}>
+                                <div className="flex items-center cursor-pointer" onClick={() => setInformationOpen(!informationOpen)}>
+                                    <span>Information</span>
+                                    <svg className={`ml-1 h-4 w-4 transition-transform duration-300 transform ${informationOpen ? 'rotate-180' : ''}`} viewBox="0 0 20 20" fill="currentColor">
+                                        <path fillRule="evenodd" d="M6.293 7.293a1 1 0 011.414 0L10 9.586l2.293-2.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clipRule="evenodd" />
+                                    </svg>
+                                </div>
+                                {informationOpen && (
+                                    <div className="absolute top-full whitespace-nowrap left-0 bg-white rounded-md shadow-lg py-1">
+                                        <Link href="/information/details" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">Details</Link>
+                                        <Link href="/information/attendance" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">Attendance</Link>
+                                    </div>
+                                )}
+                            </div>
+                            <div className="relative" ref={resultsDropdownRef}>
+                                <div className="flex items-center cursor-pointer" onClick={() => setResultsOpen(!resultsOpen)}>
+                                    <span>Results</span>
+                                    <svg className={`ml-1 h-4 w-4 transition-transform duration-300 transform ${resultsOpen ? 'rotate-180' : ''}`} viewBox="0 0 20 20" fill="currentColor">
+                                        <path fillRule="evenodd" d="M6.293 7.293a1 1 0 011.414 0L10 9.586l2.293-2.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clipRule="evenodd" />
+                                    </svg>
+                                </div>
+                                {resultsOpen && (
+                                    <div className="absolute top-full whitespace-nowrap left-0 bg-white rounded-md shadow-lg py-1">
+                                        <Link href="/results/ncea-summary" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">NCEA Summary</Link>
+                                        <Link href="/results/current-year" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">Current Year Results</Link>
+                                        <Link href="/results/class-efforts" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">Class Efforts</Link>
+                                        <Link href="/results/reports" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">Reports</Link>
+                                    </div>
+                                )}
+                            </div>
+                        </div>
                     </div>
                 </nav>
             </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,28 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <main>
-      <div className='max-w-3xl px-4 mx-auto'>
-        <h1 className='text-westlake text-4xl font-bold'>Welcome to our Parent/Student Portal</h1>
-        </div>
+      <div className='max-w-5xl mx-auto'>
+        <h1 className='text-westlake py-6 text-5xl text-left font-bold'>Welcome to our Parent/Student Portal</h1>
+
+        <p className='font-bold py-4'>On this portal you can find:</p>
+        <ul className='list-disc ml-6 space-y-1'>
+          <li>Student contact details - please request changes if incorrect</li>
+          <li>School reports</li>
+          <li>School fees (parent access only)</li>
+          <li>NCEA results summary</li>
+          <li>Student timetables and attendance information</li>
+          <li>School-related documents</li>
+        </ul>
+        <p className='font-bold py-4'>Usernames and Passwords</p>
+        <ul className='list-disc ml-6 space-y-1'>
+          <li><span className='font-bold'>Students</span>: Log in with normal username and password (username <span className='font-bold'>not</span> email)</li>
+          <li><span className='font-bold'>Caregivers</span>: Username - same as student; Password - A password has been allocated to you. These will have been emailed to you.</li>
+          <li>Forgotten or unknown password - please contact the school at <Link className='text-blue-500' href='mailto:support@westlake.school.nz'>support@westlake.school.nz</Link></li>
+        </ul>
+      </div>
     </main>
   )
 }
